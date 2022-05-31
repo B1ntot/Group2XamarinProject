d82fe7478783958c3821ed473039140ad33929c3
Calvin <bintot914@gmail.com>
2022-05-31 15:36:50 -0400

    Add project files.

---
+++ Group2XamarinProject/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Group2XamarinProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginClicked(object sender, EventArgs e)
+        {
+            if (username.Text != null && password.Text != null && username.Text != "" && password.Text != "")
+            {
+                // Check if username and password is in database and matches
+                string correctUser = "user";
+                string correctPass = "pass";
+                if (username.Text.Equals(correctUser) && password.Text.Equals(correctPass))
+                {
+                    // Go to GameLibrary Page
+                }
+                else
+                {
+                    DisplayAlert("Error", "Wrong Username or Password", "OK");
+                }
+            }
+            else
+            {
+                DisplayAlert("Error", "Missing Information", "OK");
+            }
+        }
+
+        async private void RegisterClicked(object sender, EventArgs e)
+        {
+            // Go to RegisterPage
+            await Navigation.PushModalAsync(new RegisterPage());
+        }
+    }
+}
---
+++ Group2XamarinProject/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group2XamarinProject
+{
+    public class User
+    {
+        private int userID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public User(string un, string pass)
+        {
+            username = un;
+            password = pass;
+        }
+    }
+}
